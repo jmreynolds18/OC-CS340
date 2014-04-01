f5a81253a0f05bf8d2a500a319ff64a025ad77b9
Joshua Reynolds <reyno28@email.lcc.edu>
2014-04-01 11:37:35 -0400

    made changes to readme and got binarytrees menu to work

---
+++ README.md
@@ -0,0 +1,5 @@
+# CS 340: Data Structures & Algorithms
+
+## Spring 2014
+
+My repo for my work in CS 340: Data Structures & Algorithms
--- binarytrees.c
+++ binarytrees.c
@@ -1,34 +1,62 @@
 #include <stdio.h>
 
-typedef struct{
- int num;
-}NodeData;
+// typedef struct{
+//  int num;
+// }NodeData;
 
-typedef struct treenode{
- NodeData data;
- struct treenode *left, *right;
-}TreeNode, *TreeNodePtr;
+// typedef struct treenode{
+//  NodeData data;
+//  struct treenode *left, *right;
+// }TreeNode, *TreeNodePtr;
 
-typdef struct{
- TreeNodePtr root;
-}BinaryTree;
+// typdef struct{
+//  TreeNodePtr root;
+// }BinaryTree;
 
-
-main(){
-
+void wequit(){
+	printf("We quit\n");
 }
 
-void visit(TreeNodePtr node){
- printf("%d", node -> data.num);
+int main(){
+	int menu = -1;
+
+	while(1){
+		printf("Please enter a number: ");
+		scanf("%d", &menu);
+		
+
+		if (menu == 0){
+			wequit();
+			break;
+		}
+		else if(menu == 1){
+			printf("add node\n");
+		}
+		else if(menu == 2){
+			printf("print preorder\n");
+		}
+		else if(menu == 3){
+			printf("print inorder\n");
+		}
+		else if(menu == 4){
+			printf("print postorder\n");
+		}
+		else
+			printf("%d is not a menu choice!\n",menu);
+	}
 }
 
-void inOrder(TreeNodePtr node){
- void visit(TreeNodePtr);
- if(node != NULL){
-  inOrder(node -> left);
-  visit(node);
-  inOrder(node -> right);
-}
+// void visit(TreeNodePtr node){
+//  printf("%d", node -> data.num);
+// }
+
+// void inOrder(TreeNodePtr node){
+//  void visit(TreeNodePtr);
+//  if(node != NULL){
+//   inOrder(node -> left);
+//   visit(node);
+//   inOrder(node -> right);
+// }
 
 
 
